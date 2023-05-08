4fc303628bc353b62854627360abbaa24f27327d
Monica Saeed <monicasaeed12@gmail.com>
2023-05-08 21:32:12 +0300

    Update

--- DWH_Delete.sql
+++ DWH_Delete.sql
@@ -1,3 +1,7 @@
+USE [Faculty_DWH]
 Delete From DWH_Grade;
 Delete From DWH_Graduates;
 Delete From DWH_Prof_rate;
+Delete From Best_course_rate;
+Delete From DWH_Top_Students;
+Delete From Top_Per_Department;
